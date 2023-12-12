1d4c77e198dfcf719cc6f946d3811bcf54a79d6c
Bryan Sandoval <bssandovalrod@gmail.com>
2023-12-11 21:03:16 -0600

    Update _entity.routes.js

--- src/routes/_entity.routes.js
+++ src/routes/_entity.routes.js
@@ -8,6 +8,7 @@ entityRoutes.get('/', [verifyToken], controller.getEntities);
 entityRoutes.post('/', [verifyToken], controller.createEntity);
 entityRoutes.get('/:id', [verifyToken], controller.getEntity);
 entityRoutes.put('/:id', [verifyToken], controller.updateEntity);
+entityRoutes.patch('/:id', [verifyToken], controller.patchEntity);
 entityRoutes.delete('/:id', [verifyToken], controller.deleteEntity);
 
 export const PREFIX = '/entity';
